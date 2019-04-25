7c06ee72b7df711b6499049d5ca2ac0e5a526898
Karlis <jekabsons6@gmail.com>
2019-04-25 19:23:38 +0300

    Added APTS I

---
+++ APTS I/Paldies_dyn.cpp
@@ -0,0 +1,86 @@
+#include <fstream>
+using namespace std;
+
+struct Node {
+    Node* next;
+    char* info;
+};
+
+void deleteList(Node* &first) {
+    Node* deleter = first;
+    while(deleter) {
+        first = first->next;
+        delete[] deleter->info;
+        delete deleter;
+        deleter = first;
+    }
+}
+
+bool printListToFile(Node* first, fstream &fout, bool space) {
+    bool isEmpty = true;
+    if (!space && first) fout << " ";
+    while(first) {
+        isEmpty = false;
+        fout << first->info;
+        first = first->next;
+        if (first) fout << " ";
+    }
+    return isEmpty;
+}
+
+void addToList(char* vards, Node* &first, int len) {
+    Node* newnode = new Node;
+    newnode->info = new char[len+1];
+    int counter = 0;
+    do {
+        newnode->info[counter] = vards[counter];
+        counter++;
+    } while(vards[counter-1] != '\0');
+    if (first) {
+        newnode->next = first;
+    } else {
+        newnode->next = NULL;
+    }
+    first = newnode;
+    return;
+}
+
+int main() {
+    fstream fin("paldies.in", ios::in);
+    fstream fout("paldies.out", ios::out);
+    Node* student[26] = {NULL};
+    char vards[256];
+    char c;
+    fin.get(c);
+    while(fin) {
+        int length = 0;
+        if (isalpha(c)) {
+            do {
+                vards[length] = tolower(c);
+                fin.get(c);
+                length++;
+            } while (isalpha(c) && !fin.eof() && length < 255);
+            vards[length] = '\0';
+            ///VARDS UZ LISTIEM
+            int j = (int)vards[0]-97;
+            addToList(vards, student[j], length);
+            ///VARDS AIZSUTITS UZ LISTIEM
+        } else {
+            do {
+                fin.get(c);
+            } while(!isalpha(c) && !fin.eof());
+        }
+    }
+    bool isEmpty = true;
+    for (int i = 0; i < 26; i++) {
+            if(printListToFile(student[i], fout, isEmpty) == false) {
+                isEmpty = false;
+                deleteList(student[i]);
+            }
+    }
+    if (isEmpty) {
+        fout << "nothing";
+    }
+    fin.close();
+    fout.close();
+}
