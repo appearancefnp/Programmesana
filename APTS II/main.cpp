5adf0a2c4e25491d8bdb8799af24b5f3cff65d96
Karlis <jekabsons6@gmail.com>
2019-05-14 21:10:27 +0300

    Completed APTS II
    Fully functional program.
    It can search for the shortest path in a graph. It will take amount of
    servers, add edges to them and calculate which computer will be the
    server.

--- APTS II/main.cpp
+++ APTS II/main.cpp
@@ -1,5 +1,4 @@
 #include <fstream>
-#include <iostream>
 using namespace std;
 
 class Queue {
@@ -10,9 +9,11 @@ private:
     };
     elem *first;
     elem *last;
+    int count;
 public:
     Queue() {
         first=last=NULL;
+        count = 0;
     }
     void enqueue(int val) {
         elem *p = new elem; /// s1
@@ -21,6 +22,7 @@ public:
         if(first==NULL) first=p; /// s4a
         else last->next=p; /// s4b
         last=p; /// s5
+        count++;
     }
     int dequeue() {
         int val=-1;
@@ -30,11 +32,15 @@ public:
             first = p->next; /// 2
             delete p; /// 3
             p=first; /// 4
+            count--;
         }
         return val;
     }
     bool empty() {
-        return (first == NULL);
+        return (count == 0);
+    }
+    int size() {
+        return count;
     }
     ~Queue() {
         elem *p = first; /// 1
@@ -62,11 +68,13 @@ private:
         int value;
         elem *next;
     };
+    int count;
 public:
     elem *start;
     elem *last;
     List() {
         start=last=NULL;
+        count = 0;
     }
     void push_back(int val) {
         elem *p = new elem; /// s1
@@ -75,6 +83,20 @@ public:
         if(start==NULL) start=p; /// s4a
         else last->next=p; /// s4b
         last=p; /// s5
+        count++;
+    }
+    bool empty() {
+        return (count == 0);
+    }
+    int size() {
+        return count;
+    }
+    int& operator[] (int i) {
+         elem *p = start;
+         for (int j=0; j<i; j++) {
+            p = p->next;
+         }
+         return p->value;
     }
     ~List() {
         elem *p = start; /// 1
@@ -91,14 +113,18 @@ void add_edge(List adj[], int source, int dest) {
     adj[dest].push_back(source);
 }
 
-bool BreadthFirstSearch(List adj[], int source, int dest, int vertices, int prev[], int dist[]){
+//Funkcija atgriez lielako distanci
+int BreadthFirstSearchForMaxDist(List adj[], int source, int vertices, int dist[]){
     //Izveido tuksu rindu
     Queue que;
+    //Izveido apmekletibas array
+    int max;
+    int visited[vertices];
     //Uzstada, ka vel neesam bijusi neviena virsotne
-    for (int i = 0; i < v; i++) {
+    for (int i = 0; i < vertices; i++) {
         visited[i] = false;
         dist[i] = 999999;
-        prev[i] = -1;
+        //prev[i] = -1;
     }
     //Source mes esam apmeklejusi un tas bija 0 garuma
     visited[source] = true;
@@ -106,35 +132,62 @@ bool BreadthFirstSearch(List adj[], int source, int dest, int vertices, int prev
     que.enqueue(source);
 
     // standard BFS algorithm
-    // JASAFIXO SI DALA
     while (!que.empty()) {
         int w = que.dequeue();
-        for (int i = 0; i < adj[u].size(); i++) {
-            if (visited[adj[u][i]] == false) {
-                visited[adj[u][i]] = true;
-                dist[adj[u][i]] = dist[u] + 1;
-                pred[adj[u][i]] = u;
-                queue.push_back(adj[u][i]);
-
-                // We stop BFS when we find
-                // destination.
-                if (adj[u][i] == dest)
-                   return true;
+        for (int i = 0; i < adj[w].size(); i++) {
+            if (visited[adj[w][i]] == false) {
+                visited[adj[w][i]] = true;
+                dist[adj[w][i]] = dist[w] + 1;
+                que.enqueue(adj[w][i]);
+                max = dist[adj[w][i]] ;
             }
         }
     }
+    return max;
+}
 
-    return false;
+void printServer(List adj[], int vertices, fstream &fout) {
+    //Glabâ attâlumu masîvâ lîdz katrai virsotnei. Katrai virsotnei lielakais attalums
+    int dist[vertices], maxdist[vertices];
+    int min = 999999;
+    //Atrod katrai virsotnei lielako attalumu
+    for (int i=0; i<vertices; i++) {
+        maxdist[i] = BreadthFirstSearchForMaxDist(adj, i, vertices, dist);
+        if (maxdist[i] < min) min = maxdist[i];
+    }
+    fout << min << endl;
+    bool printed = false;
+    for (int i=0; i<vertices; i++) {
+        //Parbauda vai nav izprintets jau
+        if(maxdist[i] == min) {
+            if (printed) fout << " ";
+            fout << i+1;
+            printed = true;
+        }
+    }
 }
 
 
 int main() {
+    int v = 0;
+    int source = 1,destination = 1;
+    fstream fin("serveris.in", ios::in);
+    fstream fout("serveris.out", ios::out);
     //NOLASA NO FAILA CIK BUS VIRSOTNU
-    int v = 1;
+    fin >> v;
     // IZVEIDO INCIDENCES SARAKSTU
     List adj[v];
     // LASA NO FAILA VIRSOTNES;
-    add_edge(adj, 0, 1);
-    //IZDZEST LISTUS
+    do {
+        fin >> source;
+        fin >> destination;
+        if (source == 0 and destination == 0) break;
+        add_edge(adj, source-1, destination-1);
+    } while (1);
+    //VEIC APSTRADI
+    printServer(adj,v, fout);
+
+    fin.close();
+    fout.close();
 
 }
