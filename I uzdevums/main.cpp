bb8d7f4b8ffdd9ebee64eda08690dc5d93d46e2a
Karlis <jekabsons6@gmail.com>
2019-05-15 17:48:25 +0300

    Added input and output files
    Edited colors on I uzdevums
    Added input and output files on APTS II

--- I uzdevums/main.cpp
+++ I uzdevums/main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <math.h>
-const float SIZE = 1080;
+const float SIZE = 1000;
 const double PI = 3.14159265359;
 
 void drawAstroid(float size, sf::RenderWindow &window) {
@@ -10,15 +10,17 @@ void drawAstroid(float size, sf::RenderWindow &window) {
     lines[1].position = sf::Vector2f(offset, offset);
     lines[1].color = sf::Color::White;
     for (float a = 0; a <= 2*PI; a += 0.0001) {
+        //Aprekina astroida malas punktu
         float cos3 = pow(cos(a),3);
         float sin3 = pow(sin(a),3);
         float x1 = r * cos3 + offset;
         float y1 = r * sin3 + offset;
         lines[0].position = sf::Vector2f(x1, y1);
-        if (a < PI/2)lines[0].color  = sf::Color::Red;
-        if (a > PI/2 and a < PI) lines[0].color  = sf::Color::Blue;
-        if (a > PI and a < PI*3/2) lines[0].color  = sf::Color::Green;
-        if (a > PI*3/2 and a < 2*PI) lines[0].color  = sf::Color::White;
+        //Uzstada kvadrantiem krasas
+        if (a < PI/2)lines[0].color  = sf::Color::White;
+        if (a > PI/2 and a < PI) lines[0].color  = sf::Color::Red;
+        if (a > PI and a < PI*3/2) lines[0].color  = sf::Color::White;
+        if (a > PI*3/2 and a < 2*PI) lines[0].color  = sf::Color::Red;
         window.draw(lines);
     }
 }
